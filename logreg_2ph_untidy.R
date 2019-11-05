0e524bd8ea60242fc9cd8ac9cac8549b639ca87f
Unknown <sarah.c.lotspeich@vanderbilt.edu>
2019-11-05 10:30:02 -0600

    Sped up logreg_2ph_untidy.R (zip zip)

--- logreg_2ph_untidy.R
+++ logreg_2ph_untidy.R
@@ -237,25 +237,29 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
   # Save distinct X -------------------------------------------------
   x_obs <- data.frame(unique(data[1:n_v,c(X_val)]))
   x_obs <- data.frame(x_obs[order(x_obs[,1]),])
-  colnames(x_obs) <- paste0(c(X_val), "_ref")
+  colnames(x_obs) <- c(X_val)
   m <- nrow(x_obs)
-  x_obs_stacked <- do.call(rbind, replicate(n = n, expr = x_obs, simplify = FALSE))
+  x_obs_stacked <- do.call(rbind, replicate(n = (n-n_v), expr = x_obs, simplify = FALSE))
   x_obs_stacked <- data.frame(x_obs_stacked[order(x_obs_stacked[,1]),])
-  colnames(x_obs_stacked) <- paste0(c(X_val), "_ref")
+  colnames(x_obs_stacked) <- c(X_val)
   #suppressMessages(data %<>% dplyr::left_join(data.frame(x_k, k = 1:m)))
   
-  # Save static (X*,Y*,X,Y,C) snice they don't change ---------------
+  # Save static (X*,Y*,X,Y,C) since they don't change ---------------
+  comp_dat_val <- data[c(1:n_v),c(Y_unval, X_unval, C, Bspline, X_val, Y_val)]
+  comp_dat_val <- merge(x = comp_dat_val, y = data.frame(x_obs, k = 1:m), all.x = TRUE)
+  comp_dat_val <- comp_dat_val[,c(Y_unval, X_unval, C, Bspline, X_val, Y_val, "k")]
+  comp_dat_val <- data.matrix(comp_dat_val)
   # 2 (m x n)xd matrices (y=0/y=1) of each (one column per person, --
   # one row per x) --------------------------------------------------
-  suppressWarnings(comp_dat_noY <- cbind(data[,c(Y_unval, X_unval, C, Bspline, Y_val, X_val)], x_obs_stacked))
-  comp_dat_y0 <- data.frame(comp_dat_noY, Y_ref = 0)
-  comp_dat_y1 <- data.frame(comp_dat_noY, Y_ref = 1)
-  comp_dat <- data.matrix(cbind(rbind(comp_dat_y0, comp_dat_y1), 
-                          k = rep(rep(seq(1,m), each = n), times = 2)))
+  suppressWarnings(comp_dat_unval <- cbind(data[-c(1:n_v),c(Y_unval, X_unval, C, Bspline)],
+                                           x_obs_stacked))
+  comp_dat_y0 <- data.frame(comp_dat_unval, Y = 0)
+  comp_dat_y1 <- data.frame(comp_dat_unval, Y = 1)
+  colnames(comp_dat_y0)[length(colnames(comp_dat_y0))] <- colnames(comp_dat_y1)[length(colnames(comp_dat_y1))] <- Y_val
+  comp_dat_unval <- data.matrix(cbind(rbind(comp_dat_y0, comp_dat_y1), 
+                          k = rep(rep(seq(1,m), each = (n-n_v)), times = 2)))
   
-  w_t_val <- which(comp_dat[,Y_val] == comp_dat[,"Y_ref"] & comp_dat[,X_val] == comp_dat[,paste0(X_val, "_ref")])
-  comp_dat_val <- comp_dat[w_t_val,]
-  
+  comp_dat_all <- rbind(comp_dat_val, comp_dat_unval)
   # Initialize parameter values -------------------------------------
   ## theta, gamma ---------------------------------------------------
   if(initial_lr_params == "Zero")
@@ -275,10 +279,10 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
     prev_gamma <- theta0 <- matrix(glm(formula = as.formula(paste0(Y_unval, "~", paste(c(X_unval, Y_val, X_val, C), collapse = "+"))), family = "binomial", data = data[c(1:n_v),])$coefficient, ncol = 1)
   }
   
-  theta_formula <- as.formula(paste0(paste0(Y_val, "_ref"), "~", paste(c(paste0(X_val, "_ref"), C), collapse = "+")))
-  theta_design_mat <- cbind(int = 1, comp_dat[,c(paste0(X_val, "_ref"), C)])
-  gamma_formula <- as.formula(paste0(Y_unval, "~", paste(c(X_unval, paste0(Y_val, "_ref"), paste0(X_val, "_ref"), C), collapse = "+")))
-  gamma_design_mat <- cbind(int = 1, comp_dat[,c(X_unval, "Y_ref", paste0(X_val, "_ref"), C)])
+  theta_formula <- as.formula(paste0(Y_val, "~", paste(c(X_val, C), collapse = "+")))
+  theta_design_mat <- cbind(int = 1, rbind(comp_dat_val[,c(X_val, C)], comp_dat_unval[,c(X_val, C)]))
+  gamma_formula <- as.formula(paste0(Y_unval, "~", paste(c(X_unval, Y_val, X_val, C), collapse = "+")))
+  gamma_design_mat <- cbind(int = 1, rbind(comp_dat_val[,c(X_unval, Y_val, X_val, C)], comp_dat_unval[,c(X_unval, Y_val, X_val, C)]))
   # If unvalidated variable was left blank, assume error-free -------
   ## Need to write simplification here 
   
@@ -286,24 +290,10 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
   ## Numerators sum B(Xi*) over k = 1,...,m -------------
   ## Save as p_val_num for updates ----------------------
   ## (contributions don't change) -----------------------
-  #prev_p <- p_val_num <- matrix(0, nrow = m, ncol = sn)
-  #colsum_Bspline_val <- colSums(data[c(1:n_v), Bspline])
-  
-  p_val_num <- rowsum(x = comp_dat[w_t_val,Bspline], group = comp_dat[w_t_val,"k"], reorder = TRUE)
+  p_val_num <- rowsum(x = comp_dat_val[,Bspline], group = comp_dat_val[,"k"], reorder = TRUE)
   prev_p <- p0 <-  t(t(p_val_num)/colSums(p_val_num))
-  # for (j in 1:sn)
-  # {
-  #   for (k in 1:m)
-  #   {
-  #     prev_p[k,j] <- p_val_num[k,j] <- sum(data[which(data[c(1:n_v),X_val] == x_obs[k,]),Bspline[j]])
-  #   }
-  #   prev_p[,j] <- prev_p[,j]/colsum_Bspline_val[j]
-  # }
-  # p0 <- prev_p
   
   # Initialize psi matrix to hold E-step estimates --------------------
-  # setup n x m matrix for q
-  #iter_q <- q0 <- matrix(as.numeric(comp_dat[,Y_val] == comp_dat[,"Y_ref"] & comp_dat[,X_val] == comp_dat[,"X_ref"]), nrow = nrow(comp_dat), ncol = 1)
   
   CONVERGED <- FALSE
   CONVERGED_MSG <- "Unknown"
@@ -322,30 +312,30 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
     # E Step ----------------------------------------------------------
     ## Update the psi_kyji for unvalidated subjects -------------------
     ### P(Y|X) --------------------------------------------------------
-    pY_X <- 1/(1 + exp(-as.numeric(cbind(int = 1, comp_dat[,c(paste0(X_val, "_ref"), C)]) %*% prev_theta)))
-    pY_X[which(comp_dat[,"Y_ref"] == 0)] <- 1-pY_X[which(comp_dat[,"Y_ref"] == 0)]
+    pY_X <- 1/(1 + exp(-as.numeric(theta_design_mat[-c(1:n_v),] %*% prev_theta)))
+    pY_X[which(comp_dat_unval[,Y_val] == 0)] <- 1-pY_X[which(comp_dat_unval[,Y_val] == 0)]
     ### -------------------------------------------------------- P(Y|X)
     ###################################################################
     ### P(Y*|X*,Y,X) --------------------------------------------------
-    pYstar <- 1/(1 + exp(-as.numeric(cbind(int = 1, comp_dat[,c(X_unval, "Y_ref", c(paste0(X_val, "_ref")), C)]) %*% prev_gamma)))
+    pYstar <- 1/(1 + exp(-as.numeric(gamma_design_mat[-c(1:n_v),] %*% prev_gamma)))
     pYstar[which(comp_dat[,Y_unval] == 0)] <- 1 - pYstar[which(comp_dat[,Y_unval] == 0)]
     ### -------------------------------------------------- P(Y*|X*,Y,X)
     ###################################################################
     ### p_kj ----------------------------------------------------------
-    pX <- do.call(rbind, replicate(n = n, expr = prev_p, simplify = FALSE))
+    pX <- do.call(rbind, replicate(n = (n-n_v), expr = prev_p, simplify = FALSE))
     ### need to reorder pX so that it's x1, ..., x1, ...., xm, ..., xm-
-    pX <- pX[order(rep(seq(1,m), times = n)),]
+    pX <- pX[order(rep(seq(1,m), times = (n-n_v))),]
     pX <- rbind(pX, pX)
     ### ---------------------------------------------------------- p_kj
     ###################################################################
     ### Update numerator ----------------------------------------------
     ### P(Y|X,C)*P(Y*|X*,Y,X,C)p_kjB(X*) ------------------------------
-    psi_t_num <- pY_X * pYstar * comp_dat[,Bspline] * pX
+    psi_t_num <- pY_X * pYstar * comp_dat_unval[,Bspline] * pX
     ### Update denominator --------------------------------------------
     #### Sum up all rows per id (e.g. sum over xk/y) ------------------
     #### reorder = TRUE returns them in ascending order of i ----------
     #### (rather than in order of encounter) --------------------------
-    psi_t_num_sumover_k_y <- rowsum(psi_t_num, group = rep(seq(1,n), times = 2*m))
+    psi_t_num_sumover_k_y <- rowsum(psi_t_num, group = rep(seq(1,(n-n_v)), times = 2*m))
     #### Then sum over the sn splines ---------------------------------
     #### Same ordering as psi_t_num_sumover_k_y, just only 1 column ---
     psi_t_num_sumover_k_y_j <- rowSums(psi_t_num_sumover_k_y)
@@ -361,19 +351,19 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
     w_t <- rowSums(psi_t)
     ## ---------------------- Update the w_kyi for unvalidated subjects
     ## For validated subjects, w_t = I(Xi=xk) so make them all 0 ------
-    w_t[rep(data[,Validated], 2*m)] <- 0
+    #w_t[rep(data[,Validated], 2*m)] <- 0
     ## then place a 1 in the w_t_val positions ------------------------
-    w_t[w_t_val] <- 1
+    #w_t[w_t_val] <- 1
     ## Check: w_t sums to 1 over within i -----------------------------
     # table(rowSums(rowsum(w_t, group = rep(rep(seq(1,n), times = m), times = 2))))
     
     ## Update the u_kji for unvalidated subjects ----------------------
     ## by summing over Y = 0/1 w/i each i, k --------------------------
     ## add top half of psi_t (y = 0) to bottom half (y = 1) -----------
-    u_t <- psi_t[c(1:(m*n)),] + psi_t[-c(1:(m*n)),]
+    u_t <- psi_t[c(1:(m*(n-n_v))),] + psi_t[-c(1:(m*(n-n_v))),]
     ## make u_t for the (1:n_v) validated subjects = 0 ----------------
     ## so that they won't contribute to updated p_kj ------------------
-    u_t[rep(data[,Validated], times = m),] <- 0
+    #u_t[rep(data[,Validated], times = m),] <- 0
     ## ---------------------- Update the u_kji for unvalidated subjects
     ###################################################################
     # E Step ----------------------------------------------------------
@@ -383,7 +373,8 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
     ## Update theta using weighted logistic regression ----------------
     ### Gradient ------------------------------------------------------
     mu <- theta_design_mat %*% prev_theta
-    gradient_theta <- matrix(data = c(colSums(w_t * c((comp_dat[,c("Y_ref")]-1 + exp(-mu)/(1+exp(-mu)))) * theta_design_mat)), ncol = 1)
+    w_t <- c(rep(1,n_v), w_t)
+    gradient_theta <- matrix(data = c(colSums(w_t * c((comp_dat_all[,Y_val]-1 + exp(-mu)/(1+exp(-mu)))) * theta_design_mat)), ncol = 1)
     ### ------------------------------------------------------ Gradient
     ### Hessian -------------------------------------------------------
     hessian_theta <- matrix(0, nrow = ncol(theta_design_mat), ncol = ncol(theta_design_mat), byrow = TRUE)
@@ -398,7 +389,7 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
                           })
     if (TRUE %in% is.na(new_theta))
     {
-      suppressWarnings(new_theta <- matrix(glm(formula = theta_formula, family = "binomial", data = data.frame(comp_dat), weights = w_t)$coefficients, ncol = 1))
+      suppressWarnings(new_theta <- matrix(glm(formula = theta_formula, family = "binomial", data = data.frame(comp_dat_all), weights = w_t)$coefficients, ncol = 1))
     }
     if(VERBOSE) print(new_theta)
     ### Check for convergence -----------------------------------------
@@ -407,7 +398,7 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
     ###################################################################
     ## Update gamma using weighted logistic regression ----------------
     mu <- gamma_design_mat %*% prev_gamma
-    gradient_gamma <- matrix(data = c(colSums(w_t * c((data_mat[,c(Y_unval)]-1 + exp(-mu)/(1+exp(-mu)))) * gamma_design_mat)), ncol = 1)
+    gradient_gamma <- matrix(data = c(colSums(w_t * c((comp_dat_all[,c(Y_unval)]-1 + exp(-mu)/(1+exp(-mu)))) * gamma_design_mat)), ncol = 1)
     ### ------------------------------------------------------ Gradient
     ### Hessian -------------------------------------------------------
     hessian_gamma <- matrix(0, nrow = ncol(gamma_design_mat), ncol = ncol(gamma_design_mat), byrow = TRUE)
@@ -422,7 +413,7 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
                           })
     if (TRUE %in% is.na(new_gamma))
     {
-      suppressWarnings(new_gamma <- matrix(glm(formula = gamma_formula, family = "binomial", data = data.frame(comp_dat), weights = w_t)$coefficients, ncol = 1))
+      suppressWarnings(new_gamma <- matrix(glm(formula = gamma_formula, family = "binomial", data = data.frame(comp_dat_all), weights = w_t)$coefficients, ncol = 1))
     }
     if(VERBOSE) print(new_gamma)
     # Check for convergence -----------------------------------------
@@ -432,10 +423,7 @@ TwoPhase_LogReg <- function(Y_unval=NULL, Y_val=NULL, X_unval=NULL, X_val=NULL, 
     ## Update {p_kj} --------------------------------------------------
     ### Update numerators by summing u_t over i = 1, ..., n -----------
     new_p_num <- p_val_num + 
-      rowsum(u_t, group = rep(seq(1,m), each = n), reorder = TRUE)
-    ### Update denominators by summing new_p_num w/i each col ---------
-    #new_p_denom <- matrix(rep(colSums(new_p_num), times = m), nrow = m, ncol = sn, byrow = TRUE)
-    #new_p <- new_p_num/new_p_denom
+      rowsum(u_t, group = rep(seq(1,m), each = (n-n_v)), reorder = TRUE)
     new_p <- t(t(new_p_num)/colSums(new_p_num))
     if(VERBOSE) print(new_p[1,])
     ### Check for convergence -----------------------------------------
