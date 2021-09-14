0c02ae328085ac019f64954f4cfdf96967b26344
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-09-14 13:25:28 -0400

    Delete nrst.R

--- R/nrst.R
+++
@@ -1,8 +0,0 @@
-nrst <- function(target, Bspline_coeff, k = 2) {
-  target_rep <- do.call(rbind, replicate(n = nrow(Bspline_coeff), expr = target, simplify = FALSE))
-  neighborhood <- Bspline_coeff[, 1:(ncol(Bspline_coeff) / 2)]
-  coeff <- Bspline_coeff[, -c(1:(ncol(Bspline_coeff) / 2))]
-  diff <- rowSums(abs(target_rep - neighborhood))
-  coeff_ord <- coeff[as.vector(order(diff)), ]
-  return(colMeans(coeff_ord[1:k, ]))
-}
