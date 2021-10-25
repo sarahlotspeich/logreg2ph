02beafdf0fc3d7652cbc3f8419406e88910ad9fc
Joey Sherrill <joeysherrill1387@gmail.com>
2021-10-25 13:47:47 -0500

    Generate RcppExports.R

--- simulations/Table4_SimSetup.R
+++ simulations/Table4_SimSetup.R
@@ -120,6 +120,7 @@ B[which(Xa == 0),1:(0.75 * nsieve)] <- splines::bs(x = Xbstar[which(Xa == 0)], d
 B[which(Xa == 1),(0.75 * nsieve + 1):nsieve] <- splines::bs(x = Xbstar[which(Xa == 1)], df = 0.25 * nsieve, Boundary.knots = range(Xbstar[which(Xa == 1)]), intercept = TRUE)
 colnames(B) <- paste0("bs", seq(1, nsieve))
 sdat <- cbind(sdat, B)
+library(logreg2ph)
 smle <- logreg2ph(Y_unval = "Ystar",
                   Y_val = "Y",
                   X_unval = "Xbstar",
