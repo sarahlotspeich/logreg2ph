abfb46433236a567e034843a7227419c0dc148da
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2021-10-28 16:22:43 -0400

    Remove Bspline parameter

--- R/cv_observed_data_loglik.R
+++ R/cv_observed_data_loglik.R
@@ -9,8 +9,7 @@
 #' @param X_val Column name(s) with the validated predictors. If \code{X_unval} and \code{X_val} are \code{null}, all precictors are assumed to be error-free.
 #' @param C (Optional) Column name(s) with additional error-free covariates.
 #' @param Validated Column name with the validation indicator. The validation indicator can be defined as \code{Validated = 1} or \code{TRUE} if the subject was validated and \code{Validated = 0} or \code{FALSE} otherwise.
-#' @param Bspline Vector of column names containing the B-spline basis functions.
-#' @param data A dataframe with one row per subject containing columns: \code{Y_unval}, \code{Y_val}, \code{X_unval}, \code{X_val}, \code{C}, \code{Validated}, and \code{Bspline}.
+#' @param data A dataframe with one row per subject containing columns: \code{Y_unval}, \code{Y_val}, \code{X_unval}, \code{X_val}, \code{C}, and \code{Validated}.
 #' @param theta_pred Vector of columns in \code{data} that pertain to the predictors in the analysis model.
 #' @param gamma_pred Vector of columns in \code{data} that pertain to the predictors in the outcome error model.
 #' @param TOL Tolerance between iterations in the EM algorithm used to define convergence.
@@ -19,7 +18,7 @@
 #' @export
 
 cv_observed_data_loglik <- function(q = 4, sN = 10, fold, Y_unval = NULL, Y_val = NULL, X_unval = NULL, X_val = NULL, C = NULL,
-                        Validated = NULL, Bspline = NULL, data, theta_pred = NULL, gamma_pred = NULL,
+                        Validated = NULL, data, theta_pred = NULL, gamma_pred = NULL,
                         TOL = 1E-4, MAX_ITER = 1000) {
 
   if (is.null(theta_pred)) { theta_pred <- c(X_val, C) }
@@ -54,10 +53,11 @@ cv_observed_data_loglik <- function(q = 4, sN = 10, fold, Y_unval = NULL, Y_val 
                                                                       Boundary.knots = range(data[which(data[, C] == 1), X_unval]),
                                                                       intercept = TRUE)
     }
+    colnames(B) <- paste0("bs", 1:sN)
     # Fit the SMLE to training data
     suppressMessages(
       train_fit <- logreg2ph(Y_unval = Y_unval, Y_val = Y_val, X_unval = X_unval, X_val = X_val, C = C,
-                             Validated = Validated, Bspline = Bspline, data = train,
+                             Validated = Validated, Bspline = colnames(B), data = train,
                              theta_pred = theta_pred, gamma_pred = gamma_pred,
                              noSE = TRUE, TOL = TOL, MAX_ITER = MAX_ITER)
     )
@@ -80,7 +80,7 @@ cv_observed_data_loglik <- function(q = 4, sN = 10, fold, Y_unval = NULL, Y_val 
       test_x <- data.frame(test_x[order(test_x[, 1]), ])
       colnames(test_x) <- X_val
       test_x <- cbind(k_ = 1:nrow(test_x), test_x)
-      test_p <- matrix(data = NA, nrow = nrow(test_x), ncol = length(Bspline))
+      test_p <- matrix(data = NA, nrow = nrow(test_x), ncol = ncol(colnames(B)))
 
       for (i in 1:nrow(test_x)) {
         x_ <- test_x[i, X_val]
@@ -115,7 +115,7 @@ cv_observed_data_loglik <- function(q = 4, sN = 10, fold, Y_unval = NULL, Y_val 
 
       ll_f <- observed_data_loglik(N = nrow(test), n = sum(test[, Validated]),
                                  Y_unval = Y_unval, Y_val = Y_val, X_unval = X_unval, X_val = X_val, C = C,
-                                 Bspline = Bspline, comp_dat_all = cd, theta_pred = theta_pred, gamma_pred = gamma_pred,
+                                 Bspline = colnames(B), comp_dat_all = cd, theta_pred = theta_pred, gamma_pred = gamma_pred,
                                  theta = train_theta, gamma = train_gamma, p = re_test_p)
       ll[i] <- ll_f
     }
